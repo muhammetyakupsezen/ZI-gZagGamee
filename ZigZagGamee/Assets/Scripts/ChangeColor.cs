2a05a60825198a4c1fa6eda60cd2921b1126fab6
muhammetyakupsezen <116495280+muhammetyakupsezen@users.noreply.github.com>
2023-05-05 15:25:21 +0300

    color change end

--- ZigZagGamee/Assets/Scripts/ChangeColor.cs
+++ ZigZagGamee/Assets/Scripts/ChangeColor.cs
@@ -15,6 +15,7 @@ public class ChangeColor : MonoBehaviour
     void Start()
     {
         birinciRenk = Random.Range(0,colors.Length);
+        ikinciRenk = colors[IkinciRenkSec()];
         zeminMat.color = colors[birinciRenk];
 
         Camera.main.backgroundColor = colors[birinciRenk];
@@ -23,6 +24,31 @@ public class ChangeColor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Color fark = zeminMat.color - ikinciRenk; //  renklerin rgb değerleri çıkarılabiliyor
+
+        if (Mathf.Abs(fark.r) + Mathf.Abs(fark.g) +Mathf.Abs(fark.b) <0.2f  )
+        {
+            ikinciRenk = colors[IkinciRenkSec()];
+        }
+        zeminMat.color = Color.Lerp(zeminMat.color,ikinciRenk,0.0003f); // burda farklı olan renklerden zemin rengni yani birinci rengi ikinci renge dönüştürüyor arada 0.2f lik fark kalmışken yani tam aynı renge gelmekteyken yeni renk oluşturuyorsun if in içinde
+        Camera.main.backgroundColor = Color.Lerp(Camera.main.backgroundColor,ikinciRenk,0.0007f); // 
+
     }
+
+
+    int IkinciRenkSec()
+    {
+        int ikinciRenkIndex;
+        ikinciRenkIndex = Random.Range(0, colors.Length);
+
+        while (ikinciRenkIndex == birinciRenk)
+        {
+            ikinciRenkIndex = Random.Range(0, colors.Length);
+        }
+
+
+        return ikinciRenkIndex;
+    }
+
+
 }
